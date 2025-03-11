b279585dd19a3266e6f7a607078d1967dadde8eb
Luan <luan.caue0302@gmail.com>
2025-03-11 20:09:48 -0300

    Atualização de arquivos do projeto

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -1,20 +1,29 @@
+// API significa Application Programming Interface
+// POST, GET, PUT, DELETE
+// CRUD - Create Read Update Delete
+// Endpoint
+// Middleware
+
 import express from "express";
-import { artistsArray } from "../../front-end/src/assets/database/artists.js";
-import { songsArray } from "../../front-end/src/assets/database/songs.js";
+import cors from "cors";
+import { db } from "./connect.js";
 
 const app = express();
 const PORT = 3001;
 
+app.use(cors());
+// app.use(express.json());
+
 app.get("/", (request, response) => {
-  response.send("Olá, Mundo, agora oii");
+  response.send("Só vamos trabalhar com os endpoints '/artists' e '/songs'");
 });
 
-app.get("/artists", (request, response) => {
-  response.send(artistsArray);
+app.get("/artists", async (request, response) => {
+  response.send(await db.collection("artists").find({}).toArray());
 });
 
-app.get("/songs", (request, response) => {
-  response.send(songsArray);
+app.get("/songs", async (request, response) => {
+  response.send(await db.collection("songs").find({}).toArray());
 });
 
 app.listen(PORT, () => {
